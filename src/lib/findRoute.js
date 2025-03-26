6538b5a9d2c6a371f0698ba94e8c6186ef694620
dinesh_kuniyal <dineshkuniyal0@gmail.com>
2025-03-27 03:30:07 +0530

    Refactored the code

--- src/lib/findRoute.js
+++ src/lib/findRoute.js
@@ -2,36 +2,50 @@ const findRoute = (route, segments, method) => {
   const [segment, ...restSegments] = segments;
 
   if (!segment) {
-    return route.handlers.has(method) ? route : null;
+    return isMethodSupported(route, method) ? route : null;
   }
 
-  const isParent = route.isParent(segment);
-  const isLast = restSegments.length === 0;
-  if (!isParent) {
-    const dynamicChild = route.dynamicChild();
-    const wildcardRoute = route.findChild('*');
-
-    if (wildcardRoute && isLast)
-      return findRoute(wildcardRoute, restSegments, method);
-
-    if (dynamicChild) {
-      const dynamicRoute = findRoute(dynamicChild, restSegments, method);
-      dynamicRoute.urlData = {
-        ...dynamicRoute?.urlData,
-        [dynamicChild.path.slice(1)]: segment
-      };
-
-      return dynamicRoute;
-    }
-
-    if (wildcardRoute)
-      return findRoute(wildcardRoute, restSegments, method);
-
-    return null;
-  };
+  if (!route.isParent(segment)) {
+    return handleNonParentRoute(route, segment, restSegments, method);
+  }
 
   const child = route.findChild(segment);
   return findRoute(child, restSegments, method);
+};
+
+const isMethodSupported = (route, method) => {
+  return route.handlers.has(method);
+};
+
+const handleNonParentRoute = (route, segment, restSegments, method) => {
+  const dynamicChild = route.dynamicChild();
+  const wildcardRoute = route.findChild('*');
+  const isLast = restSegments.length === 0;
+
+  if (wildcardRoute && isLast) {
+    return findRoute(wildcardRoute, restSegments, method);
+  }
+
+  if (dynamicChild) {
+    return handleDynamicChild(dynamicChild, segment, restSegments, method);
+  }
+
+  if (wildcardRoute) {
+    return findRoute(wildcardRoute, restSegments, method);
+  }
+
+  return null;
+};
+
+const handleDynamicChild = (dynamicChild, segment, restSegments, method) => {
+  const dynamicRoute = findRoute(dynamicChild, restSegments, method);
+  if (dynamicRoute) {
+    dynamicRoute.urlData = {
+      ...dynamicRoute?.urlData,
+      [dynamicChild.path.slice(1)]: segment
+    };
+  }
+  return dynamicRoute;
 };
 
 export default findRoute;
